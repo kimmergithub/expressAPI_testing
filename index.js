73520d44f8a87fc0c8fc899152a69b52132f04fc
kimmergithub <kimmer@atomicprofits.com>
2017-08-09 15:27:55 -0700

    fixed and added in the use method

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
-
+const routes = require('./routes/api'):
 const express = require('express');
 
 const app = express();
+
+// this tell express that we want to use our routes from our routes page on api.js... the use method.
+app.use(routes);
 
 app.get('/', function(){
 	console.log('GET request!');
