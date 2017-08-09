81fb1561e35090cd6035b2b090946656f195a664
kimmergithub <kimmer@atomicprofits.com>
2017-08-09 14:58:27 -0700

    threw in my ports and stuff

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function(){
+	console.log('GET request!');
+})
+
 app.listen(process.env.port || 3000, function(){
 	console.log('now listening on app.listen!');
 });
