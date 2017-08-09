bff9c43ab5302ad9f1a044605369bdbb012c07ea
kimmergithub <kimmer@atomicprofits.com>
2017-08-09 16:01:19 -0700

    fixed the req res order

--- routes/api.js
+++ routes/api.js
@@ -4,22 +4,22 @@ const express = require('express');
 const router = express.Router();
 
 // get a list of ninjas from the db
-router.get('/ninjas', function(res, req){
+router.get('/ninjas', function(req, res){
   res.send({type: 'GET'});
 });
 
 // add a new ninja
-router.post('/ninjas', function(res, req){
+router.post('/ninjas', function(req, res){
   res.send({type: 'POST'});
 });
 
 // update ninja in db
-router.put('/ninjas/:id', function(res, req){
+router.put('/ninjas/:id', function(req, res){
   res.send({type: 'PUT'});
 });
 
 // delete ninja from the db
-router.delete('ninjas/:id', function(res, req){
+router.delete('ninjas/:id', function(req, res){
   res.send({type: 'DELETE'});
 });
 
