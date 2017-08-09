9afde2a9e6ef5668279ea7e8f2705eece77ce0bf
kimmergithub <kimmer@atomicprofits.com>
2017-08-09 16:03:21 -0700

    put the api in origal route designation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // this tell express that we want to use our routes from our routes page on api.js... the use method.
-app.use(routes);
+app.use('/api', routes);
 
 app.get('/', function(){
 	console.log('GET request!');
